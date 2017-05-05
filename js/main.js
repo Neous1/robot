a25693c696c40f28dcf9c8638e7e354ba73c95db
Neous1 <Neous1@users.noreply.github.com>
2017-05-05 14:19:05 -0400

    updated index.html, uploaded map1.json added map to stateMain.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onload = function()
 {
     if (screen.width>1500)
 	{
-      game=new Phaser.Game(480,640,Phaser.AUTO,"ph_game");
+      game=new Phaser.Game(640,480,Phaser.AUTO,"ph_game");
 	}
 	else
 	{		
--- js/stateMain.js
+++ js/stateMain.js
@@ -1,14 +1,43 @@
 var StateMain = {
 
     preload: function () {
+        game.load.spritesheet("robot", "images/main/robot.png", 80,111,28);
+        game.load.image("tiles","images/tiles.png");
+        game.load.tilemap("map1", "images/maps/map1.json",null,Phaser.Tilemap.TILED_JSON);
 
     },
 
     create: function () {
-        console.log("Ready!");
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //load map
+        debugger;
+        this.map = game.add.tilemap("map1");
+        this.map.addTilesetImage("tiles");
+
+        this.layer = this.map.createLayer("Tile Layer 1");
+        this.layer.resizeWorld(); // 
+        this.map.setCollisionBetween(0, 24); // keeping robot inbound
+
+
+        this.robot = game.add.sprite(150,150,"robot");
+        this.robot.animations.add("idle", [0,1,2,3,4,5,6,7,8,9],12,true);
+        this.robot.animations.add("walk", [10,11,12,13,14,15,16,17],12,true);
+        this.robot.animations.add("jump", [18,19,20,21,22,23,24,25],12,false);
+
+
+        this.robot.animations.play("idle");
+        this.robot.anchor.set(.5, .5);
+        game.physics.arcade.enable(this.robot);
+        this.robot.body.gravity.y = 100;
+        this.robot.body.bounce.set(.25);
+        this.robot.body.collideWorldBound = true;
+
+        game.camera.follow(this.robot);
     },
 
     update: function () {
+        game.physics.arcade.collide(this.robot, this.layer);
 
     }
 
